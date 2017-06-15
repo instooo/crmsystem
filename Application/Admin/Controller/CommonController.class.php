052e15d726ed7046b088fc7b429d18cd616416c1
qf19910623 <qf19910623@gmail.com>
2017-06-16 00:07:29 +0800

    公共控制器

---
+++ Application/Admin/Controller/CommonController.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qf19910623
+ * Date: 2017/6/16 0016
+ * Time: 0:04
+ */
+namespace Admin\Controller;
+
+use Think\Controller;
+
+class CommonController extends Controller {
+
+    public function _initialize() {
+
+    }
+
+}
--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -1,7 +1,7 @@
 <?php
 namespace Admin\Controller;
 use Think\Controller;
-class IndexController extends Controller {
+class IndexController extends CommonController {
     //主页
     public function index(){
         $this->display();
