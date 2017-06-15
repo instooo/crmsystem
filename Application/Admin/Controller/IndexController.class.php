e5ca1381300afa89d5439f2a66767b6f1037adb9
qf19910623 <qf19910623@gmail.com>
2017-06-16 00:01:43 +0800

    搭建整体布局

---
+++ Application/Admin/Controller/IndexController.class.php
@@ -0,0 +1,24 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+class IndexController extends Controller {
+    //主页
+    public function index(){
+        $this->display();
+    }
+
+    //左侧栏
+    public function left() {
+        $this->display();
+    }
+
+    //顶部栏
+    public function top() {
+        $this->display();
+    }
+
+    //首页
+    public function main() {
+        $this->display();
+    }
+}
---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+    'APP_SUB_DOMAIN_DEPLOY' =>  true,   // 是否开启子域名部署
+    'APP_SUB_DOMAIN_RULES'  =>  array(
+        'www.crm.me' =>  'Home',
+        'admin.crm.me' =>  'Admin'
+    ), // 子域名部署规则
+
+);
