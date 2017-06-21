d1fd807a4c1c2ce5addaf1571e48efcb03380696
qf1991623 <qf1991623@gmail.com>
2017-06-21 13:56:59 +0800

    更换数据库

--- Application/Common/Conf/dbconfig.php
+++ Application/Common/Conf/dbconfig.php
@@ -7,10 +7,11 @@
  */
 return array(
     'DB_TYPE'               =>  'mysql',     // 数据库类型
-    'DB_HOST'               =>  '211.149.201.127', // 服务器地址
+    'DB_HOST'               =>  '118.193.220.117', // 服务器地址
+    //'DB_HOST'               =>  '211.149.201.127', // 服务器地址
     'DB_NAME'               =>  'crmsystem',          // 数据库名
     'DB_USER'               =>  'crmsystem',      // 用户名
     'DB_PWD'                =>  'crmsystem',          // 密码
-    'DB_PORT'               =>  '3306',        // 端口
+    'DB_PORT'               =>  '1036',        // 端口
     'DB_PREFIX'             =>  'crm_',    // 数据库表前缀
 );
