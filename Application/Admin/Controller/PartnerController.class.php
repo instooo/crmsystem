e0ae8a877a7d4c4adbd5f2ae01c7c4a19ee0fa1b
qf1991623 <qf1991623@gmail.com>
2017-06-22 18:08:14 +0800

    字段管理静态页面

---
+++ Application/Admin/Controller/PartnerController.class.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * 客户管理控制器
+ * Created by qinfan qf19910623@gmail.com.
+ * Date: 2017/6/22
+ */
+namespace Admin\Controller;
+use Think\Controller;
+
+class PartnerController extends CommonController {
+    public $partnerConfig;
+
+    public function _initialize() {
+        parent::_initialize();
+        $this->partnerConfig = include(CONF_PATH.'partner.config.php');
+    }
+
+    /**
+     * 字段管理
+     */
+    public function fieldsManager() {
+        $this->assign('fields_type_list', $this->partnerConfig['FIELDS_TYPE']);
+        $this->display();
+    }
+}
---
+++ Application/Common/Conf/partner.config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 客户配置
+ * */
+return array(
+    //字段配置
+    'FIELDS_TYPE'   =>  array(
+        'partner'   =>  '客户',
+        'contact'   =>  '联系人',
+        'agreement'   =>  '合同',
+    )
+);
