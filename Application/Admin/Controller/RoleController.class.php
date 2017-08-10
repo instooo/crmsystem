1c81b25e97fa32189a35ab1257fc501abe3ccb71
dxl <710190573@qq.com>
2017-08-10 17:53:38 +0800

    工作流修改

--- Application/Admin/Controller/RoleController.class.php
+++ Application/Admin/Controller/RoleController.class.php
@@ -18,6 +18,7 @@ class RoleController extends CommonController {
 		$uidarr = explode('|',$uid);
 		$act=$_GET['act'];
 		$extend_tit=$_GET['extend_tit'];
+		$data_field=$_GET['data_field'];		
 		//查询出对应的职位和对应的用户，数量比较少，忽略性能
 		$role = M('role')->field('id,name,pid')->select();
 		//查询出所有的用户，数量比较少，忽略性能
@@ -45,10 +46,19 @@ class RoleController extends CommonController {
 		//查找对应的action
 		$actresult = M('workflow_action')->select();
 		$this->assign('actresult',$actresult);
-		//查询出所有可选字段
+		//查询出所有可选字段,并查询出已被选着的字段
 		$fields = M('fields')->where('`field_type`="agreement"')->select();		
+		$h_field = explode('|',$data_field);
+		foreach($fields as $key=>$val){
+			if(in_array($val['id'],$h_field)){
+				$fields[$key]['is_select']=1;
+			}else{
+				$fields[$key]['is_select']=0;
+			}
+		}
 		$this->assign('fields',$fields);
-		$this->assign('extend_tit',$extend_tit);
+		$this->assign('extend_tit',$extend_tit);		
+		$this->assign('data_field',$data_field);
 		$this->assign('title_html',$title_html);
 		$this->assign('wid',$wid);
 		$this->assign('json_data',$json_data);
--- Application/Admin/Controller/WorkflowController.class.php
+++ Application/Admin/Controller/WorkflowController.class.php
@@ -160,21 +160,116 @@ class WorkflowController extends CommonController {
 
 	//编辑节点
 	public function edit(){
-		$data['wid'] = I('get.wid'); 			
-		if (!$data['wid']) {
-			$ret['code'] = -2;
-			$ret['msg'] = '参数不全';
-			break;
-		}
-		$map['w_id']=$data['wid'];			
-		$result = M('workflow')->where($map)->find(); 
-		if($result){			
-			$extend = M('workflow_extend')->where($map)->order('e_id desc')->select();			
-			$result['child'] = $extend;			
+		if(IS_POST){
+			$ret = array('code'=>-1,'msg'=>'');
+			do{				
+				//[step] => steps1:user1|user2,steps2:user1
+				//[acts] => steps1:act1|act2,steps2:act1
+				$data = array();
+				$data['w_id'] = I('post.w_id'); 
+				$data['w_name'] = I('post.w_name'); 
+				$data['w_createtime']=time();
+				//处理流程的几个步骤
+				$steps=I('post.step');
+				$acts=rtrim(I('post.act'),',');	
+				$des=rtrim(I('post.des'),',');
+				$field=rtrim(I('post.field'),',');	
+				
+				$steps_arr = explode(',',$steps);
+				$acts_arr = explode(',',$acts);			
+				$dess_arr = explode(',',$des);
+				$field_arr = explode(',',$field);
+				if (!$data['w_name']) {
+					$ret['code'] = -2;
+					$ret['msg'] = '参数不全';
+					break;
+				}			
+				$map['w_id']=$data['w_id'];			
+				$l = M('workflow')->where($map)->find();         
+				if (!$l) {
+					$ret['code'] = -3;
+					$ret['msg'] = '流程不存在';
+					break;
+				} 			
+				$rs = M('workflow')->where($map)->save($data);			
+				if (!$rs) {
+					$ret['code'] = -4;
+					$ret['msg'] = '保存失败';
+					break;
+				}
+				//添加流程步骤
+				$extend_data=array();
+				foreach($steps_arr as $key=>$val){
+					$tmp_ar=explode(':',$val);	
+					$extend_data[$key]['step_id']=str_replace('steps','',$tmp_ar[0]);
+					$extend_data[$key]['uid']=str_replace('user','',$tmp_ar[1]);
+					$extend_data[$key]['status']=1;
+					$extend_data[$key]['create_time']=time();
+					$extend_data[$key]['child_wid']=0;
+					$extend_data[$key]['w_id']=$data['w_id'];
+				}				
+				foreach($acts_arr as $key=>$val ){
+					$tmp_ar=explode(':',$val);					
+					$extend_data[$key]['step_id']=str_replace('steps','',$tmp_ar[0]);
+					$extend_data[$key]['action']=str_replace('user','',rtrim($tmp_ar[1],'|'));
+					$extend_data[$key]['status']=1;
+				}
+				foreach($dess_arr as $key=>$val ){
+					$tmp_ar=explode(':',$val);					
+					$extend_data[$key]['step_id']=str_replace('steps','',$tmp_ar[0]);
+					$extend_data[$key]['des']=str_replace('user','',rtrim($tmp_ar[1],'|'));				
+				}
+				foreach($field_arr as $key=>$val ){
+					$tmp_ar=explode(':',$val);					
+					$extend_data[$key]['step_id']=str_replace('steps','',$tmp_ar[0]);
+					$extend_data[$key]['field']=str_replace('user','',rtrim($tmp_ar[1],'|'));	
+				}									
+				$map['w_id']=$data['w_id'];			
+				$extend = M('workflow_extend')->where($map)->find(); 
+				if ($extend) {
+				   M('workflow_extend')->where($map)->delete();
+				} 
+				$result = M('workflow_extend')->addAll($extend_data);
+				$ret['code'] = 1;
+				$ret['msg'] = '修改成功';
+				break;
+			}while(0);
+			exit(json_encode($ret));
 		}else{
-			echo "流程不存在";die;
+			$data['wid'] = I('get.wid'); 			
+			if (!$data['wid']) {
+				$ret['code'] = -2;
+				$ret['msg'] = '参数不全';
+				break;
+			}
+			$map['w_id']=$data['wid'];			
+			$result = M('workflow')->where($map)->find(); 
+			if($result){	
+				//查找所有用户
+				$usertmp = M('user')->select();
+				$userarr = array();
+				foreach($usertmp as $key=>$val){
+					$userarr[$val['user_number']]=$val;	
+				}			
+				$extend = M('workflow_extend')->where($map)->order('step_id asc')->select();
+				foreach($extend as $key=>$val){
+					$exuser = explode('|',$val['uid']);				
+					foreach($exuser as $k=>$v){
+						$extend[$key]['username'] .= $userarr[$v]['nickname'].",";
+					}
+				}			
+				$result['child'] = $extend;			
+			}else{
+				echo "流程不存在";die;
+			}	
+			//print_r($result);die;
+			//print_r($extend);die;
+			$this->assign('result',$result);
+			$length = count($extend)*130+55;
+			$this->assign('length',$length);
+			
+			$this->display();	
 		}
-		$this->assign('result',$result);		
-		$this->display();
+		
 	}
 }
